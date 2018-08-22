2ae744e839f27776048e94c0cf327f8eabaf0389
lishengxu <lsx1@meitu.com>
2018-08-22 10:32:05 +0800

    add test code

--- main/correct.py
+++ main/correct.py
@@ -9,7 +9,7 @@ def train(features):
         model[each] += 1
     return model
 
-NWORDS = train(words(open('../Gutenberg.txt', 'r').read()));
+NWORDS = train(words(open('../Gutenberg.txt').read()));
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
